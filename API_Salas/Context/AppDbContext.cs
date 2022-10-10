bfaf10b51610260670de2b710bcb7c4178207fe1
Vitor da Silva Gonçalves <vitordsgoncalves@gmail.com>
2022-10-09 22:29:31 -0300

    Gente, não dá mais. Dei burnout, chega, estressei demais e nem dá tempo de terminar

--- API_Salas/Context/AppDbContext.cs
+++ API_Salas/Context/AppDbContext.cs
@@ -13,18 +13,11 @@ namespace API_Alunos.Context {
         public DbSet<Impressao>? Impressoes { get; set; }
         public DbSet<Limpeza>? Limpezas { get; set; }
         public DbSet<MaterialEscritorio>? MateriaisEscritorios { get; set; }
-<<<<<<< HEAD
         public DbSet<Objeto>? Objetos { get; set; }
-        // public DbSet<Disponibilidade>? Disponibilidades { get; set; }
         
-=======
 
-        public DbSet<Objeto>? Objetos { get; set; }
-
->>>>>>> 378f4e4211e58a277a306e11be0b9ec907da8cbd
         public DbSet<Periodo>? Periodos { get; set; }
         public DbSet<Endereco>? Enderecos { get; set; }
-        // public DbSet<Disponibilidade>? Disponibilidades { get; set; }   
         public DbSet<Servico>? Servicos { get; set; }
         public DbSet<Reserva>? Reservas { get; set; }
 
